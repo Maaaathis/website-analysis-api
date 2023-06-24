1698235cbee1f33ffc8641f632def643124186a3
toohard2explain <levihsmn@gmail.com>
2023-06-24 11:10:21 +0200

    more technologies

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import WebsiteAnalyzer from './WebsiteAnalyzer';
 
 const analyzer = new WebsiteAnalyzer({
-    url: 'https://team-panel.com'
+    url: 'https://webpack.js.org'
 });
 
 analyzer.analyze().then((data) => {;
